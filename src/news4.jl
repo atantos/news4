9f8972aac43807431f04345c79f25a2715a4c422
Alexandros <atantos@gmail.com>
2020-04-13 16:52:15 +0300

    First tests
    First tests for linear function my_f()

--- src/news4.jl
+++ src/news4.jl
@@ -1,5 +1,10 @@
 module news4
 
+include("extra_file.jl")
+export my_f
+
 greet() = print("Hello World!")
+
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using news4
 using Test
 
+# either this if i don't export the function with the export keyword
+#news4.my_f(2,3)
+
+# or directly if there is an export statement.
+my_f(2,3)
+
+# adding a test for 2x + 3y (if that was the case)
+@test my_f(2,3) == 13
+
 @testset "news4.jl" begin
     # Write your own tests here.
+    @test my_f(2,3) == 13
+    @test my_f(1,2) == 8
+    @test my_f(3,4) == 18
+
 end
